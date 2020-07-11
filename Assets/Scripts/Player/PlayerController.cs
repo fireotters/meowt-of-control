c7ec2e872a852e07e5d077c1ac0eec6824161b4a
Cameron <cam.crosse97@outlook.com>
2020-07-11 15:24:29 +1000

    Bullet emitter rotates to mouse
    I gave it some thought. Player should probably be able to control the exact direction of the gun. If it's locked to the axis in a game where AI can approach from any angle, it would get annoying to use the last resort weapon.
    
    The bullet emitter now rotates to face the cursor.
    Placeholder art easier to interpret.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,42 +1,46 @@
 ﻿using System;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float movementSpeed = 1;
-
-    private Rigidbody2D _rigidbody2D;
-    private Vector2 _movementVector;
     private SpriteRenderer _sprite;
-
-    [SerializeField] private GameObject emitterPos;
-
     private Animator _animator;
-    
-    private float _currentSpeed;
 
+    // Rigidbody Movement
+    private Rigidbody2D _rigidbody2D;
+    [SerializeField] private float movementSpeed = 1;
+    private float _currentSpeed;
     private const string HorizontalAxis = "Horizontal";
     private const string VerticalAxis = "Vertical";
+    private Vector2 _movementVector;
+
+    // Weapon Aiming
+    private Camera _cam;
+    private Vector2 mousePos;
+    private Transform bulletEmitter;
+    
 
     private void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
         _sprite = GetComponent<SpriteRenderer>();
-        
-        if (emitterPos == null)
-        {
-            Debug.LogError("No emitter pos, can't shoot!");
-        }
+        _cam = Camera.main;
+
+        bulletEmitter = transform.GetChild(0);
     }
 
     private void Update()
     {
+        // Current movement vector, set character sprite
         _movementVector.x = Input.GetAxis(HorizontalAxis);
         _movementVector.y = Input.GetAxis(VerticalAxis);
-        
-        emitterPos.transform.position = SetEmitterPos();
+
+        // Current mouse position
+        mousePos = _cam.ScreenToWorldPoint(Input.mousePosition);
 
         _animator.SetFloat(HorizontalAxis, _movementVector.x);
         _animator.SetFloat(VerticalAxis, _movementVector.y);
@@ -52,31 +56,6 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    private Vector3 SetEmitterPos()
-    {
-        var position = transform.position;
-        var result = Vector3.zero;
-        
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            result = new Vector3(position.x, _sprite.bounds.extents.y, 0f);
-        }
-        else if (Input.GetKeyDown(KeyCode.S))
-        {
-            result = new Vector3(position.x, -_sprite.bounds.extents.y, 0f);
-        }
-        else if (Input.GetKeyDown(KeyCode.A))
-        {
-            result = new Vector3(_sprite.bounds.extents.x, position.y, 0f);
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            result = new Vector3(_sprite.bounds.extents.x, position.y, 0f);
-        }
-
-        return result;
-    }
-
     private void FixedUpdate()
     {
         _currentSpeed = movementSpeed;
@@ -87,5 +66,11 @@ public class PlayerController : MonoBehaviour
         );
         
         _animator.SetFloat("Speed", _rigidbody2D.velocity.magnitude);
+
+        // Bullet emitter direction
+        Vector2 lookDir = mousePos - _rigidbody2D.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        Vector3 rotationDir = new Vector3(0, 0, angle);
+        bulletEmitter.rotation = Quaternion.Euler(rotationDir);
     }
 }
