148a75e485f4ed92eb0abcd9672cfd622ba230f7
Cameron <cam.crosse97@outlook.com>
2020-08-19 20:26:41 +1000

    Last last changes: Enemy Spawning
    Enemy Spawning
    - Enemy spawn rate starts at one per 6sec, reduces by .5sec every day. Max rate reached at Day 9, enemy every 2sec
    - Sanic spawnrate starts at 21% on Day 2, max at 40% by Day 10. Details are in EnemySpawnerController comments
    - Big Chungus spawning delays the next spawn by an additional 8sec
      - Also, made the big chungus cap actually work, oops
    - Spawner can no longer send nothing during a spawn attempt

--- Assets/Scripts/Enemy/EnemySpawnerController.cs
+++ Assets/Scripts/Enemy/EnemySpawnerController.cs
@@ -10,6 +10,7 @@ public class EnemySpawnerController : MonoBehaviour
     [SerializeField] private EnemySpawner[] _enemySpawners = default;
     private EnemySpawner _chosenSpawner;
     private int bigChungusSpawned = 0, bigChungusCap;
+    private int basicChance, sanicChance, bigChungusChance;
 
     private void Start()
     {
@@ -25,7 +26,7 @@ public class EnemySpawnerController : MonoBehaviour
     {
         if (Time.time > nextSpawn && _gM.enemyNumberSpawned < _gM.enemyMaxCount)
         {
-            // Iterate spawn rate and GameManager's spawned enemy count
+            // Iterate spawn rate
             nextSpawn = Time.time + spawnRate;
 
             // Choose one of four directions for the new enemy to come from
@@ -33,34 +34,58 @@ public class EnemySpawnerController : MonoBehaviour
             _chosenSpawner = _enemySpawners[randSpawner];
 
             // Determine which enemy to spawn
-            int randEnemy = Random.Range(0, 30);
-            if (randEnemy == 0 && _gM.currentRound >= 4) // Spawn Big Chungus (1/30 chance)
+            int randEnemy = Random.Range(1, 101);
+
+            // Spawn Big Chungus
+            if (randEnemy <= bigChungusChance)
             {
                 if (bigChungusSpawned < bigChungusCap)
                 {
+                    bigChungusSpawned++;
+                    nextSpawn += 8f; // Big Chungus spawn delays further spawns
                     _chosenSpawner.SpawnEnemy(GameAssets.i.enemyBigChungus);
                     _gM.enemyNumberSpawned++;
                 }
             }
-            else if (randEnemy < 5 && _gM.currentRound >= 2) // Spawn Sanic (4/30 chance)
+            // Spawn Sanic
+            else if (randEnemy <= sanicChance + bigChungusChance)
             {
                 _chosenSpawner.SpawnEnemy(GameAssets.i.enemySanic);
                 _gM.enemyNumberSpawned++;
             }
-            else if (randEnemy < 27) // Spawn Basic (22/30 chance)
+            // Spawn Basic
+            else
             {
                 _chosenSpawner.SpawnEnemy(GameAssets.i.enemyBasic);
                 _gM.enemyNumberSpawned++;
             }
-
-            // Nothing spawns at all (3/30 chance)
         }
     }
 
-    public void SetBigChungusCap()
+    public void PerWaveChanges()
     {
         bigChungusSpawned = 0;
         bigChungusCap = _gM.currentRound / 2 - 1;
-        //Debug.Log(bigChungusCap);
+
+
+        // Big Chungus Chance (Day 1-3: 0% |    | Day 4+: 5%)
+        bigChungusChance = _gM.currentRound >= 4 ? 5 : 0;
+
+        /* Sanic Chance (Day 1: 0% |    | Day 2-9: 10% + 3% * DayNum (Day 2-3 adding Big Chungus' 5%) |    | Day 10+: 40%) */
+        if (_gM.currentRound >= 2)
+        {
+            sanicChance = 10 + _gM.currentRound * 3;
+            sanicChance += 5 - bigChungusChance;
+            if (sanicChance > 40) sanicChance = 40;
+        }
+        else { sanicChance = 0; }
+
+        /* Basic Chance (Day 1: 100% |    | Day 2-9: Remainder of other two. 79% to 58% |    | Day 10+: 55%) */
+        basicChance = 100 - bigChungusChance - sanicChance;
+
+        string bigChungusString = $"Big Chungus: {bigChungusChance}%";
+        string sanicString = $"Sanic: {sanicChance}%";
+        string basicString = $"Basic: {basicChance}%";
+        Debug.Log($"Spawn chances this wave: {bigChungusString} ||| {sanicString} ||| {basicString}");
     }
 }
